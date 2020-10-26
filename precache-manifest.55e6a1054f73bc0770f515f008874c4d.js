573a2e9d63ad2007448d78505f1aaa97a08b4c6e
OrigenData <cristian.mcgrathbds@gmail.com>
2020-10-25 17:06:04 -0700

    Updates

---
+++ precache-manifest.55e6a1054f73bc0770f515f008874c4d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20d5fd727fce0bf711cadd767ea428c7",
+    "url": "/page1-react/index.html"
+  },
+  {
+    "revision": "3a29be3ba3e8cc13e615",
+    "url": "/page1-react/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "2d687ca543b8b327a3be",
+    "url": "/page1-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3a29be3ba3e8cc13e615",
+    "url": "/page1-react/static/js/2.9d49d1a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/page1-react/static/js/2.9d49d1a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d687ca543b8b327a3be",
+    "url": "/page1-react/static/js/main.cab1c6d7.chunk.js"
+  },
+  {
+    "revision": "41f4af03c7485db4184a",
+    "url": "/page1-react/static/js/runtime-main.d7c7b351.js"
+  },
+  {
+    "revision": "96aff77c5833d91511a87f051dc63231",
+    "url": "/page1-react/static/media/logo.96aff77c.96aff77c.jpg"
+  }
+]);
